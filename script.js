58a6541199760da03aade6da8e7335c290e8d047
ishuduwal <115239975+ishuduwal@users.noreply.github.com>
2023-03-06 20:26:38 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function ChangeBackground() {
+    document.body.style.background = "#3dcfd3";
+}
+function ChangeSecond() {
+    document.body.style.background = "#1d9bf0";
+}
+function ChangeThird() {
+    document.body.style.background = "#c70b0b";
+}
+function ChangeFourth() {
+    document.body.style.background = "#d6d9db";
+}
+function ChangeFifth() {
+    document.body.style.background = "#2696e0";
+}
